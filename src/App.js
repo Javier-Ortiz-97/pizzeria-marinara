9c363d2200aabf81ff073d9d84503e33f940b9a8
Javier <javifuego6@gmail.com>
2022-05-26 23:53:30 -0700

    Updated dev logo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
 import Menu from "./Menu";
-import "./App.css"; 
+import "./App.css";
 import Logo from "./images/logo.png";
+import JaviLogo from "./images/javilogo.png";
 function App() {
   return (
     <div className="body">
       <div className="header">
-      <img key={Logo} src={Logo} alt={""} className={"logo"} />
+        <img key={Logo} src={Logo} alt={""} className={"logo"} />
         <h1>Pizzeria Marinara</h1>
       </div>
 
       <Menu />
-      <footer className="font-two spacing">
+      <footer className="font-two spacing row center-items">
         {" "}
-        &copy; 2022 Pizzeria Marinara All Rights Reserved, Website by Javier
+        &copy; 2022 Pizzeria Marinara All Rights Reserved, Website by{" "}
+        <img key={JaviLogo} src={JaviLogo} alt={""} className={"javi-logo"}/>
       </footer>
     </div>
   );
